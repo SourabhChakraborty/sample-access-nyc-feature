63c543a16a1c013e473bf9ea9caa08e0be67f895
Sourabh Chakraborty <8957837+SourabhChakraborty@users.noreply.github.com>
2022-06-15 12:11:19 -0400

    added Autofac dependency injection

---
+++ sampleAccessNycProject/sampleAccessNycProject/Startup/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Autofac;
+using sampleAccessNycProject.ApplicationLogic;
+using sampleAccessNycProject.Models;
+
+namespace sampleAccessNycProject.Startup
+{
+	public class Startup
+	{
+		public void ConfigureContainer(ContainerBuilder builder)
+		{
+			builder.RegisterType<SNAPFormRenewalRetriever>().Keyed<FormType>(FormType.SNAP);
+		}
+	}
+}
+
---
+++ sampleAccessNycProject/sampleAccessNycProject/Startup/WebApplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Autofac.Extensions.DependencyInjection;
+
+namespace sampleAccessNycProject.Startup
+{
+    public class WebApplication
+    {
+        // using example from
+        // https://medium.com/dotnet-hub/use-autofac-ioc-container-in-dotnet-or-aspnetcore-autofac-dependency-injection-netcore-ffad19d87163
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
+
